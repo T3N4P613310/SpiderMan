6e5f3825e2c4204ac7ec3e3b59c184154d7d88f9
Nathan Munn <the.3null.4player.6.13.3.10.@gmail.com>
2018-02-04 18:42:48 -0400

    Initial Commit
    Commiting work previously made.
    note: this crawler is not designed for conent scraping it is designed for mapping the web to a graph.

---
+++ main.py
@@ -0,0 +1,39 @@
+from time import sleep
+from bs4 import BeautifulSoup
+import ssl
+import urllib2
+import mechanize
+import re
+import networkx
+
+pat = re.compile('^(http(.)?://)([^/])*?([^.])*?(.htm(l)?|/)?$')  # DNT!!!
+seed = unicode('http://info.cern.ch/')  # The home of the first page on the web.
+queue = [seed]
+links = {seed}
+G = networkx.MultiGraph()
+while len(queue) > 0:
+    browser = mechanize.Browser()
+    browser.set_handle_robots(False)
+    browser.set_handle_refresh(False)
+    browser.addheaders = [('User-agent', "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36")]  # DNT!!!
+    print "next: " + queue[0]
+    try:
+        browser.open(queue[0], timeout=1200)
+        soup = BeautifulSoup(browser.response().read(), "html5lib")
+        # soup.prettify()
+        for k in soup.find_all('a'):
+            lnk = unicode(k.get('href'))
+            if pat.match(lnk):
+                # print k.get('href')
+                G.add_edge(queue[0], lnk)
+                if lnk not in links:
+                    queue.append(lnk)
+                    links.add(lnk)
+    except (mechanize.HTTPError, mechanize.URLError, ssl.CertificateError, ssl.SSLError, urllib2.URLError) as error:
+        print "\t" + str(error)
+        # pass
+    finally:
+        networkx.draw_networkx(G)
+        browser.close()
+    del queue[0]
+    sleep(0.001)
